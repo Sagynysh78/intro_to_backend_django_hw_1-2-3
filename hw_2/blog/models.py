cf69393066f4dba79503aca9ef5debba33e4bd9f
Сагыныш Рахимбекова <sagynysh@Air-Sagynys.lan>
2025-01-25 20:14:36 +0500

    Homework 2

---
+++ hw_2/blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=1000)
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
